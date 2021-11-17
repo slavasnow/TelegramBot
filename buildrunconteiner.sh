788ea62ffdd3345c0f4f6e6b7472fa43c77a15c0
slavasnow <51907565+slavasnow@users.noreply.github.com>
2021-11-17 12:05:57 +0300

    добавлена команда для просмотра всех контейнеров

--- buildrunconteiner.sh
+++ buildrunconteiner.sh
@@ -2,22 +2,33 @@
 
 wget https://github.com/slavasnow/TelegramBot/releases/latest/download/TelegramBot
 
+echo "=============="
 echo "скачивание завершено"
+echo "=============="
 
 mkdir TGBOT
 mv TelegramBot TGBOT
 
+echo "=============="
 echo "перемещенение завершено"
+echo "=============="
 
 podman build -t bot-image .
 
+echo "=============="
 echo "сборка образа заврешена"
+echo "=============="
 
 podman run -t -d --rm --name bot localhost/bot-image:latest
 
+echo "=============="
 echo "запуск завершен заврешена"
+echo "=============="
 
 rm -R TGBOT
 
+echo "=============="
 echo "работа окончена"
+echo "=============="
 
+podman ps -a
