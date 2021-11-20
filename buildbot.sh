98dc0531ced800398989566b9ead3a21487bc661
slavasnow <51907565+slavasnow@users.noreply.github.com>
2021-11-20 10:34:50 +0300

    Изменение сборки бота

--- buildbot.sh
+++ buildbot.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-dotnet publish -c release -r linux-x64
+#dotnet publish -c release -r linux-x64
+
+dotnet publish -c release -r linux-x64 -p:PublishSingleFile=true -p:SelfContained=true
