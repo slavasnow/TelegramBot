75444d942553065d17fe76104f8b25d96e223d73
slavasnow <51907565+slavasnow@users.noreply.github.com>
2021-11-20 10:43:08 +0300

    Глобальные изменения
    - Выполнен перезд на новую кодовую базу
    - Код переписан с учетом нового стандарта
    - Добавлена функция для нового сервиса

--- src/Program.cs
+++ src/Program.cs
@@ -1,66 +1,93 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// See https://aka.ms/new-console-template for more information
 using System.Net;
+
 using Telegram.Bot;
-using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 
-namespace TelegramBot
+
+
+var client = new TelegramBotClient("1922620877:AAHd6M7t1DAAhnG6XhwymQmdYgjJmUZv0wc"); //токен для подключения 
+using var cts = new CancellationTokenSource();
+
+var receiverOption = new ReceiverOptions
 {
-    class Program
+    AllowedUpdates = { }
+};
+
+client.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOption, cancellationToken: cts.Token);
+
+var me = await client.GetMeAsync();
+
+Console.WriteLine("==========>> Ожидание сообщений <<===========");
+Console.ReadLine();
+
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient client, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message!.Type != MessageType.Text)
     {
-        private static string tocken { get; set; } = "";
-        private static TelegramBotClient client;
+        return;
+    }
 
-        static void Main(string[] args)
-        {
-            Console.WriteLine("==========>> Ожидание сообщений <<===========");
-            client = new TelegramBotClient(tocken);
-            client.StartReceiving();
-            
-            client.OnMessage += OnMassageHandler;
-            
-            
-            Console.ReadLine();
-            client.StopReceiving();
-            //Console.WriteLine("Hello World!");
-        }
+    var chatId = update.Message.Chat.Id;
+    var messageText = update.Message.Text;
+    string ip = "";
 
-        private static async void OnMassageHandler(object sender, MessageEventArgs e)
-        {
-            var msg = e.Message;
-            if (msg.Text != null)
-            {
-                switch (msg.Text)
+
+    ReplyKeyboardMarkup getButton = new(new []
+    {
+        new KeyboardButton[] { "Мой IP", "Jenkins" },
+    })
+    {
+        ResizeKeyboard = true
+    };
+
+
+    switch (messageText)
                 {
                     case "/start":
                         Console.WriteLine("Начало работы");
-                        await client.SendTextMessageAsync(msg.Chat.Id, $"Что тебе нужно?", replyMarkup: GetButtons()); //ответ на сообщение например
+                        await client.SendTextMessageAsync(chatId, $"Что тебе нужно?",replyMarkup: getButton ,cancellationToken: cancellationToken); //ответ на сообщение например
                         return;
                     
                     case "Мой IP": // определения моего ip
-                        Console.WriteLine($"Пришло сообщение: {msg.Text}"); //отображение сообщения в консоли
-                        string myip = new WebClient().DownloadString("http://icanhazip.com/");
-                        Console.WriteLine(myip);
-                        await client.SendTextMessageAsync(msg.Chat.Id, $"{myip}"); //ответ на сообщение например
+                        Console.WriteLine($"Пришло сообщение: {messageText}"); //отображение сообщения в консоли
+                        ip = new WebClient().DownloadString("http://icanhazip.com/");
+                        Console.WriteLine(ip);
+                        await client.SendTextMessageAsync(chatId, $"{ip}"); //ответ на сообщение например
                         return;
                     
+                    case "Jenkins": // определения моего ip
+                        Console.WriteLine($"Пришло сообщение: {messageText}"); //отображение сообщения в консоли
+                        ip = new WebClient().DownloadString("http://icanhazip.com/");
+                        string jenkinsip = String.Concat(ip, ":8880");
+                        Console.WriteLine(jenkinsip);
+                        await client.SendTextMessageAsync(chatId, $"{jenkinsip}"); //ответ на сообщение например
+                        return;
+
                     default:
-                        await client.SendTextMessageAsync(msg.Chat.Id, msg.Date.ToString()); //ответ на сообщение например
-                        Console.WriteLine($"Пришло сообщение: {msg.Text}"); //отображение сообщения в консоли
+                        await client.SendTextMessageAsync(chatId, update.Message.Date.ToString()); //ответ на сообщение например
+                        Console.WriteLine($"Пришло сообщение: {messageText}"); //отображение сообщения в консоли
                         return;
                 }
-            }
-        }
-        private static  IReplyMarkup GetButtons()
-        {
-            return new ReplyKeyboardMarkup
-            {
-                Keyboard = new List<List<KeyboardButton>>
-                {
-                    new List<KeyboardButton>{new KeyboardButton {Text = "Мой IP"}}
-                }
-            };
-        }
-    }
+
+}
+
+
+Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+        _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
 }
